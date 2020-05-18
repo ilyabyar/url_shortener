ce9e7d73f3b2b1bdb9d940680fe62acb9a86fa03
Ilya Berinchik <ilyabyar@gmail.com>
2020-05-18 11:32:12 +0300

    Make CSS working on heroku.

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -4,9 +4,7 @@
 
 import React from "react";
 import { render } from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import $ from 'jquery';
-// import Popper from 'popper.js';
+import './stylesheets/application'
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
 
